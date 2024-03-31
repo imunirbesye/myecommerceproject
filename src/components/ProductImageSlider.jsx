561d66ee65922acdb8db3d4286456b196485c626
Münir BESYE <imunirbesye@gmail.com>
2024-03-31 15:59:40 +0300

    I've done desktop view. Gonna continue with router links.

--- src/components/ProductImageSlider.jsx
+++ src/components/ProductImageSlider.jsx
@@ -7,66 +7,12 @@ import coverImage from "../assets/images/singleProductCover.jpg";
 const slideImages = [
   {
     url: "../assets/images/image1.jpg",
-    content: (
-      <div className="w-full flex flex-col justify-center lg:w-[70rem] lg:flex-row lg:self-center lg:justify-between lg:items-center">
-        <div className="w-full flex flex-col text-light-text-color items-center self-start mb-8 lg:w-[27rem] lg:items-start">
-          <span className="font-semibold">SUMMER 2020</span>
-          <span className="w-48 text-4xl font-bold text-center mt-8 lg:w-full lg:text-start">
-            NEW COLLECTION
-          </span>
-          <span className="w-[16rem] text-xl text-center mt-8 lg:w-full lg:text-start">
-            We know how large objects will act, but things on a small scale.
-          </span>
-          <div>
-            <button className="w-44 leading-[3rem] rounded-md font-semibold bg-success-text-color mt-6 lg:ml-6">
-              ADD TO CART
-            </button>
-          </div>
-        </div>
-      </div>
-    ),
   },
   {
     url: "../assets/images/image1.jpg",
-    content: (
-      <div className="w-full flex flex-col justify-center lg:w-[70rem] lg:flex-row lg:self-center lg:justify-between lg:items-center ">
-        <div className="w-full flex flex-col text-light-text-color items-center self-start mb-8 lg:w-[27rem] lg:items-start">
-          <span className="font-semibold">SUMMER 2020</span>
-          <span className="w-48 text-4xl font-bold text-center mt-8 lg:w-full lg:text-start">
-            NEW COLLECTION
-          </span>
-          <span className="w-[16rem] text-xl text-center mt-8 lg:w-full lg:text-start">
-            We know how large objects will act, but things on a small scale.
-          </span>
-          <div>
-            <button className="w-44 leading-[3rem] rounded-md font-semibold bg-success-text-color mt-6 lg:ml-6">
-              ADD TO CART
-            </button>
-          </div>
-        </div>
-      </div>
-    ),
   },
   {
     url: "../assets/images/image1.jpg",
-    content: (
-      <div className="w-full flex flex-col justify-center lg:w-[70rem] lg:flex-row lg:self-center lg:justify-between lg:items-center ">
-        <div className="w-full flex flex-col text-light-text-color items-center self-start mb-8 lg:w-[27rem] lg:items-start">
-          <span className="font-semibold">SUMMER 2020</span>
-          <span className="w-48 text-4xl font-bold text-center mt-8 lg:w-full lg:text-start">
-            NEW COLLECTION
-          </span>
-          <span className="w-[16rem] text-xl text-center mt-8 lg:w-full lg:text-start">
-            We know how large objects will act, but things on a small scale.
-          </span>
-          <div>
-            <button className="w-44 leading-[3rem] rounded-md font-semibold bg-success-text-color mt-6 lg:ml-6">
-              ADD TO CART
-            </button>
-          </div>
-        </div>
-      </div>
-    ),
   },
 ];
 
@@ -74,16 +20,10 @@ export default function ProductImageSlider() {
   return (
     <div className="">
       <Slide>
-        {slideImages.map((slideImage, index) => (
-          <div key={index}>
-            <div
-              className={`flex items-center bg-slate-600 bg-cover h-[30vh] content-end font-montserrat lg:h-[70vh] lg:content-center lg:items-center lg:justify-center bg-top`}
-              style={{ backgroundImage: `url(${coverImage})` }}
-            >
-              {slideImage.content}
-            </div>
-          </div>
-        ))}
+        <div
+          className="flex items-center bg-slate-600 bg-cover h-[25vh] content-end font-montserrat lg:h-[30vh] lg:content-center lg:items-center lg:justify-center bg-top"
+          style={{ backgroundImage: `url(${coverImage})` }}
+        ></div>
       </Slide>
     </div>
   );
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+
 import Header from "../layout/Header";
 import AboutProduct from "../layout/AboutProduct";
 import ProductMoreDetail from "../layout/ProductMoreDetail";
 import BestSellerProduct from "../layout/BestSellerProducts";
+import Footer from "../layout/Footer";
 
 export default function ProductPage() {
   return (
-    <div className="bg-light-gray-1">
+    <div className="bg-light-gray-1 flex flex-col items-center">
       <Header />
       <AboutProduct />
       <ProductMoreDetail />
       <BestSellerProduct />
+      <Footer />
     </div>
   );
 }
