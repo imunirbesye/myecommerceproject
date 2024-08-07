f8b51755912d18aeff71a311913fba49553d5ae3
Münir BESYE <imunirbesye@gmail.com>
2024-08-07 22:12:03 +0300

    setup for pagination

--- src/layout/ProductList.jsx
+++ src/layout/ProductList.jsx
@@ -4,12 +4,14 @@ import Filter from "../components/Filter";
 import ProductCard from "../components/ProductCard";
 import { useSelector } from "react-redux";
 import ContentLoader from "react-content-loader";
+import PaginationC from "../components/PaginationC";
 
 export default function ProductList() {
   const fetchState = useSelector((store) => store.product.fetchState);
   let products = new Array();
   products = useSelector((store) => store.product.productList);
   products.sort((a, b) => a.id - b.id);
+
   return (
         <div className="flex flex-col items-center bg-light-background-color">
             <Filter />
@@ -38,13 +40,7 @@ export default function ProductList() {
         )}
             </div>
             <div className="flex flex-row items-center justify-center my-12 mb-20">
-                <ul className="flex flex-row">
-                    <li className="px-6 py-5 text-muted-text-color font-bold bg-[#F3F3F3] border-[1px] border-muted-text-color rounded-l-md">First</li>
-                    <li className="px-4 py-5 text-primary-text-color font-bold bg-light-background-color border-[1px] border-l-[0px] border-muted-text-color">1</li>
-                    <li className="px-4 py-5 text-primary-text-color font-bold bg-light-background-color border-[1px] border-l-[0px] border-muted-text-color">2</li>
-                    <li className="px-4 py-5 text-primary-text-color font-bold bg-light-background-color border-[1px] border-l-[0px] border-muted-text-color">3</li>
-                    <li className="px-6 py-5 text-muted-text-color font-bold bg-[#F3F3F3] border-[1px] border-l-0 border-muted-text-color rounded-r-md">Next</li>
-                </ul>
+        <PaginationC />
             </div>
         </div>
     );
