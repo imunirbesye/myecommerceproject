6e7e74d22b6d38a3ccc5f19b458c05c240416955
Münir BESYE <imunirbesye@gmail.com>
2024-08-07 22:10:34 +0300

    setup for pagination in productList

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import ProductCategories from "../layout/ProductCategories";
 import ProductList from "../layout/ProductList";
 import ClientList from "../layout/ClientList";
+import { useParams } from "react-router-dom";
 
 export default function ProductListPage() {
+  let { page, gender, filter, category } = useParams();
+
+  console.log(page + " " + gender + " " + filter + " " + category);
+
   return (
     <div className="bg-light-gray-1">
       <ProductCategories />
